346160c33efb2450bc3299e2c7751f280856e4be
Adriel Rosanelli <62855784+adrielrosanelli@users.noreply.github.com>
2022-01-24 16:48:25 -0300

    teste

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,19 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { CorpoHomeComponent } from './pages/corpo-home/corpo-home.component';
-
-
+import { HeaderComponent } from './pages/header/header.component';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzImageModule } from 'ng-zorro-antd/image';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 
 @NgModule({
   declarations: [
-    CorpoHomeComponent
+    HeaderComponent,
+    CorpoHomeComponent,
   ],
   imports: [
     CommonModule,
-
+    NzPageHeaderModule,
+    NzButtonModule,
+    NzImageModule,
+    NzIconModule,
+    NzDropDownModule
   ],
   exports: [
-    CorpoHomeComponent
+    CorpoHomeComponent,
+    HeaderComponent,
   ]
 })
 export class HomeModule { }
