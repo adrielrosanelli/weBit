7bb4a81475a7fdb3a960e71ca5d7b3e3d05dcfba
Adriel Rosanelli <62855784+adrielrosanelli@users.noreply.github.com>
2022-01-24 17:57:17 -0300

    Editando o cabeçalho

---
+++ src/src/app/home/pages/corpo-home/corpo-home.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+// import Swiper styles
+import 'swiper/css';
+import Swiper, { Navigation, Pagination } from 'swiper';
+@Component({
+  selector: 'corpo-home',
+  templateUrl: './corpo-home.component.html',
+  styleUrls: ['./corpo-home.component.scss']
+})
+export class CorpoHomeComponent implements OnInit {
+
+  constructor() {
+    Swiper.use([Navigation, Pagination]);
+  }
+
+  ngOnInit(): void {
+    const swiper = new Swiper('.swiper', {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+      },
+
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+      // And if we need scrollbar
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+    });
+  }
+
+}
