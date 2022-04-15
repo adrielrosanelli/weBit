e8c8960810644078469f03e90fff1e095e2c147e
Adriel Rosanelli <adriel16-smo@hotmail.com>
2022-04-15 12:48:16 -0300

    Arrumando a responsividade no mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'weBit';
+  public windowHeigth: number = 0;
+  public windowWidth: number = 0;
+
+  ngOnInit(): void {
+    this.onResize();
+  }
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.windowHeigth = window.innerHeight;
+    this.windowWidth = window.innerWidth;
+    if (this.windowWidth > 700) {
+      document.getElementById('degrade')?.setAttribute('style', 'display: flex;flex-direction: column;height: 100%');
+    } else {
+      document.getElementById('degrade')?.setAttribute('style', 'display: flex ;flex-direction: column;height: auto;min-height:100%;background-size:cover');
+    }
+  }
 }
