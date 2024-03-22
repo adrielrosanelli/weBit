3a3f8b3094211face763120a431edf1b398130a4
adriel16smo <adriel16smo@gmail.com>
2024-03-22 18:14:30 +0000

    Nova pagina de serviços

---
+++ .idx/dev.nix
@@ -0,0 +1,34 @@
+{pkgs}: {
+  channel = "stable-23.11";
+  packages = [
+    pkgs.nodejs
+  ];
+  idx.extensions = [
+    "angular.ng-template"
+  ];
+  
+  idx.previews = {
+    enable = true;
+    previews = [
+      {
+        command = [
+          "npm"
+          "run"
+          "start"
+          "--"
+          "--port"
+          "$PORT"
+          "--host"
+          "0.0.0.0"
+          "--disable-host-check"
+        ];
+        manager = "web";
+        id = "web";
+      }
+      {
+        id = "ios";
+        manager = "ios";
+      }
+    ];
+  };
+}
