501acbea9525149e11ba572e448d94e1d2ba2643
adriel16smo <adriel16smo@gmail.com>
2024-06-05 12:24:28 +0000

    Novas alterações

--- src/app/services/valor.service.ts
+++ src/app/services/valor.service.ts
@@ -35,55 +35,60 @@ export class ValorService {
       lado: 'left',
       descricao: 'Instalação de infraestrutura',
       rota: 'instalacaoInfraestrutura',
-      conteudo: `A instalação de infraestrutura de redes é o processo de criar uma rede de computadores para permitir a comunicação e compartilhamento de dados entre dispositivos.
-      Isso inclui o planejamento, design, instalação e configuração dos componentes da rede. É importante considerar fatores como segurança, desempenho e escalabilidade ao planejar
-      e implementar uma infraestrutura de rede. É necessário ter conhecimentos técnicos avançados de rede e seguir as melhores práticas para garantir uma instalação bem-sucedida e eficiente.`
+      //conteudo: `A instalação de infraestrutura de redes é o processo de criar uma rede de computadores para permitir a comunicação e compartilhamento de dados entre dispositivos.
+      //Isso inclui o planejamento, design, instalação e configuração dos componentes da rede. É importante considerar fatores como segurança, desempenho e escalabilidade ao planejar
+      //e implementar uma infraestrutura de rede. É necessário ter conhecimentos técnicos avançados de rede e seguir as melhores práticas para garantir uma instalação bem-sucedida e eficiente.`
+      conteudo: `A instalação de redes de computadores oferece benefícios como compartilhamento de recursos, comunicação eficiente, acesso à internet, segurança de dados e colaboração entre equipes`
     },
     {
       lado: 'right',
       descricao: 'Cable management',
       rota: 'cableManagement',
-      conteudo: `O cable management é o processo de organização e gestão dos cabos em uma instalação de rede ou sistema de computador.
-      Isso inclui a rotulação, agrupamento e fixação dos cabos para garantir a estética, 
-      acessibilidade e manutenção da instalação. O cable management também é importante para melhorar a ventilação e evitar danos aos cabos.
-      Uma boa gestão de cabos pode melhorar a aparência da instalação, facilitar a manutenção e garantir a integridade dos sinais de dados transmitidos.`
+      //conteudo: `O cable management é o processo de organização e gestão dos cabos em uma instalação de rede ou sistema de computador.
+      //Isso inclui a rotulação, agrupamento e fixação dos cabos para garantir a estética, 
+      //acessibilidade e manutenção da instalação. O cable management também é importante para melhorar a ventilação e evitar danos aos cabos.
+      //Uma boa gestão de cabos pode melhorar a aparência da instalação, facilitar a manutenção e garantir a integridade dos sinais de dados transmitidos.`
+      conteudo: `O cable management melhora a circulação do ar e reduz o acúmulo de poeira, proporcionando uma organização eficiente e prevenindo danos aos cabos e equipamentos.`
     },
     {
       lado: 'left',
       descricao: 'Varredura de rede',
       rota: 'varreduraRede',
-      conteudo: `A varredura de rede é o processo de escaneamento de uma rede para identificar e coletar informações sobre os dispositivos conectados.
-      Isso inclui endereços IP, fabricantes, modelos, sistemas operacionais e outros detalhes.
-      A varredura de rede é comumente usada para verificar a segurança da rede,
-      encontrar dispositivos desatualizados ou vulneráveis e monitorar mudanças na topologia da rede.`
+//      conteudo: `A varredura de rede é o processo de escaneamento de uma rede para identificar e coletar informações sobre os dispositivos conectados.
+     // Isso inclui endereços IP, fabricantes, modelos, sistemas operacionais e outros detalhes.
+      //A varredura de rede é comumente usada para verificar a segurança da rede,
+     // encontrar dispositivos desatualizados ou vulneráveis e monitorar mudanças na topologia da rede.`
+     conteudo: `A varredura de rede é o processo de escaneamento de uma rede para identificar e coletar informações sobre os dispositivos conectados.`
     },
     {
       lado: 'right',
       descricao: 'Consultoria para provedores',
       rota: 'consultoriaProvedores',
-      conteudo: `A consultoria para provedores de serviços é um tipo de serviço de assessoria para empresas que oferecem serviços como, provedores de internet, provedores de serviços em nuvem, provedores de serviços de segurança cibernética, entre outros.
-      A consultoria pode ajudar essas empresas a melhorar sua eficiência, expandir sua base de clientes, aumentar sua receita e resolver problemas técnicos e de negócios. 
-      A consultoria pode incluir serviços como análise de mercado, planejamento estratégico, soluções de TI personalizadas, treinamento e suporte técnico.`
+      //conteudo: `A consultoria para provedores de serviços é um tipo de serviço de assessoria para empresas que oferecem serviços como, provedores de internet, provedores de serviços em nuvem, provedores de serviços de segurança cibernética, entre outros.
+      //A consultoria pode ajudar essas empresas a melhorar sua eficiência, expandir sua base de clientes, aumentar sua receita e resolver problemas técnicos e de negócios. 
+      //A consultoria pode incluir serviços como análise de mercado, planejamento estratégico, soluções de TI personalizadas, treinamento e suporte técnico.`
+      conteudo:'A varredura de redes identifica dispositivos e vulnerabilidades, permitindo a manutenção e proteção eficazes do ambiente de rede.'
     },
     {
       lado: 'left',
       descricao: 'Consultoria para reestruturação e melhoria de redes',
       rota: 'reestruturacaoMelhoriaRedes',
-      conteudo: `A consultoria para reestruturação e melhoria de redes é um serviço de assessoria para empresas que buscam melhorar sua infraestrutura de rede.
-      Isso pode incluir uma revisão da topologia da rede, da segurança da rede, da escalabilidade e da eficiência, bem como a identificação de oportunidades para melhorias.
-      Um consultor para reestruturação e melhoria de redes pode ajudar a empresa a identificar pontos fracos na rede e propor soluções para melhorar a segurança, o desempenho e a escalabilidade da rede.
-      A consultoria também pode incluir treinamento para os funcionários da empresa sobre as melhores práticas de gestão de rede e suporte para implementação das soluções propostas.
-      É importante escolher um consultor com conhecimento avançado da tecnologia de rede e experiência em reestruturação e melhoria de redes.`
+      //conteudo: `A consultoria para reestruturação e melhoria de redes é um serviço de assessoria para empresas que buscam melhorar sua infraestrutura de rede.
+      //Isso pode incluir uma revisão da topologia da rede, da segurança da rede, da escalabilidade e da eficiência, bem como a identificação de oportunidades para melhorias.
+      //Um consultor para reestruturação e melhoria de redes pode ajudar a empresa a identificar pontos fracos na rede e propor soluções para melhorar a segurança, o desempenho e a escalabilidade da rede.
+      //A consultoria também pode incluir treinamento para os funcionários da empresa sobre as melhores práticas de gestão de rede e suporte para implementação das soluções propostas.
+      //É importante escolher um consultor com conhecimento avançado da tecnologia de rede e experiência em reestruturação e melhoria de redes.`
+      conteudo:'A consultoria de redes de computadores pode otimizar infraestruturas, melhorar a eficiência operacional, identificar oportunidades de crescimento e fortalecer a segurança.'
     },
     {
       lado: 'right',
       descricao: 'Troca de equipamentos de rede em geral',
       rota: 'trocaEquipamentos',
-      conteudo: `A troca de equipamentos de rede em geral refere-se ao processo de substituição de componentes obsoletos ou danificados da rede por novos equipamentos.
-      Isso pode incluir dispositivos como roteadores, switches, firewalls, access points, cabos e outros componentes da rede. 
-      A troca de equipamentos de rede é uma parte importante da manutenção da rede e pode melhorar significativamente o desempenho, a segurança e a escalabilidade da rede. 
-      No entanto, a troca de equipamentos de rede também pode ser complicada e requer conhecimento técnico avançado para evitar interrupções na rede ou perda de dados.`
-    },
+      //conteudo: `A troca de equipamentos de rede em geral refere-se ao processo de substituição de componentes obsoletos ou danificados da rede por novos equipamentos.
+      //Isso pode incluir dispositivos como roteadores, switches, firewalls, access points, cabos e outros componentes da rede. 
+      //A troca de equipamentos de rede é uma parte importante da manutenção da rede e pode melhorar significativamente o desempenho, a segurança e a escalabilidade da rede. 
+      //No entanto, a troca de equipamentos de rede também pode ser complicada e requer conhecimento técnico avançado para evitar interrupções na rede ou perda de dados.`
+    conteudo:'A troca de equipamentos de redes de computadores pode melhorar a velocidade, segurança e confiabilidade da rede, impulsionando a produtividade e o desempenho geral do sistema.'},
   ];
 
   constructor() { }
